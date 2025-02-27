26d32736a6bfe26901ea417e15f16f317b2e5ac0
felipevinicios <felipe.1577@outlook.com>
2025-02-27 17:47:28 -0300

    Exercicio terreno

---
+++ Aula02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double largura, comprimento, precoMetroQuadrado, area, preco;
+
+            Console.Write("Digite a largura: ");
+            largura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture );
+
+            Console.Write("Digite o comprimento: ");
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Digite o preco do metro quadrado: ");
+            precoMetroQuadrado = double.Parse((Console.ReadLine()), CultureInfo.InvariantCulture);
+
+            area = largura * comprimento;
+            preco = area * precoMetroQuadrado;
+
+            Console.WriteLine("area = "+ area.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("preco = " + preco.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
